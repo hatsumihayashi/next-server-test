2bf1bbfd822d5556b01838ae406009ca48a66c87
Hatsumi Hayashi <hayashihatsumi0114@gmail.com>
2023-11-19 01:33:12 +0900

    get users data from API route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,19 @@
-export default function Page() {
-  return <h1>Hello, Next.js!</h1>;
+import { User } from "@/types/user";
+
+export default async function Page() {
+  const res = await fetch("http://localhost:3000/api/users");
+  const users: User[] = await res.json();
+
+  return (
+    <div>
+      {users.map((user) => {
+        return (
+          <div>
+            <h1>{user.name}</h1>
+            <div>ID: {user.id}</div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
